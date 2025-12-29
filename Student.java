b50b2e4950aad9e50d5e135dc6cffcdce83d1bd8
Tholiranjani <tholiranjani2007@gmail.com>
2025-12-29 12:51:01 +0530

    Student.java

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int id;
+    String name;
+    int age;
+
+    Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
